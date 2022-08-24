4cedc58d9fb3dddccd5add818ea2fc7823b2cc45
SowjanyaKannedhari <94524833+SowjanyaKannedhari@users.noreply.github.com>
2022-08-24 13:26:28 +0530

    Update c1-versions.tf

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -15,7 +15,11 @@ terraform {
 
 # Provider Block
 provider "azurerm" {
- features {}          
+ features {}     
+ subcription_id = "4a099035-a60e-4ee0-8401-bf4732aa4f03"
+ tenant_id = "f70e97ab-3864-41c2-b909-696600678e02"
+ client_id = "23df4105-4dea-4f8c-b0a0-670424cbdc7e"
+ client_secret = "-28NZq9~.EK1rquKb-5_-jVJZ1Hy7Ijm8n"
 }
 
 
